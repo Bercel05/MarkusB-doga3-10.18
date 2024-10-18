a8975e9a0144bb72367e733c236f70b0d8af16a0
MarkusBercel <markusb@kkszki.hu>
2024-10-18 10:33:49 +0200

    Project is ready

--- Program.cs
+++ Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MarkusB_doga3_10._18
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        const int diakSzam = 5;
+        string[] diakNevek = new string[diakSzam];
+        int[] pontszamok = new int[diakSzam];
+
+        for (int i = 0; i < diakSzam; i++)
         {
+            Console.Write($"Kérem, adja meg a {i + 1}. diák nevét: ");
+            diakNevek[i] = Console.ReadLine();
+
+            Console.Write($"Kérem, adja meg a(z) {diakNevek[i]} pontszámát: ");
+            pontszamok[i] = int.Parse(Console.ReadLine());
+        }
+
+       
+        double atlag = 0;
+        int legmagasabb = pontszamok[0];
+        int legalacsonyabb = pontszamok[0];
+        string legmagasabbDiak = diakNevek[0];
+        string legalacsonyabbDiak = diakNevek[0];
+
+        for (int i = 0; i < diakSzam; i++)
+        {
+            atlag += pontszamok[i];
+
+            if (pontszamok[i] > legmagasabb)
+            {
+                legmagasabb = pontszamok[i];
+                legmagasabbDiak = diakNevek[i];
+            }
+            if (pontszamok[i] < legalacsonyabb)
+            {
+                legalacsonyabb = pontszamok[i];
+                legalacsonyabbDiak = diakNevek[i];
+            }
+        }
+
+        atlag /= diakSzam; 
+
+       
+        Console.WriteLine($"\nÁtlagpontszám: {atlag:F2}");
+        Console.WriteLine($"Legmagasabb pontszám: {legmagasabb} (Diák: {legmagasabbDiak})");
+        Console.WriteLine($"Legalacsonyabb pontszám: {legalacsonyabb} (Diák: {legalacsonyabbDiak})");
+
+        
+        foreach (var pontszam in pontszamok)
+        {
+            if (pontszam < 50)
+            {
+                Console.WriteLine("Van elégtelen pontszám!");
+                break;
+            }
         }
     }
 }
